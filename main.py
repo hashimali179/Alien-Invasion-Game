f56c4b654a814473c094be8872b431b0c1c5210d
Hashim Ali Zaidi <62440904+hashimali179@users.noreply.github.com>
2021-12-26 16:20:36 +0530

    adding alien

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from  alien import Alien
 
 
 class AlienInvasion:
@@ -15,6 +16,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self.create_fleet()
+
+
+    def create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def run_game(self):
         while True:
@@ -28,7 +37,7 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         """print(len(self.bullets))"""
-        
+
     def _check_events(self):
         for event in pygame.event.get():
             """if event.type == pygame.QUIT:
@@ -68,6 +77,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
 
